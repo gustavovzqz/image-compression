987436b3d536b609fbdee0c70014bfd15a6d73f2
Gustavo Vazquez <gustavovzqz.dev@gmail.com>
2025-11-14 15:30:32 -0300

    chore and test: gitignore and benchmark

--- main/main.cpp
+++ main/main.cpp
@@ -1,30 +1,17 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include <vector>
 
 int main() {
-    // Load image
-    cv::Mat img = cv::imread("image.png", cv::IMREAD_COLOR);
-    if (img.empty()) {
-        std::cerr << "Failed to open image\n";
-        return 1;
-    }
+  // Load image
+  cv::Mat img = cv::imread("benchmark.bmp", cv::IMREAD_COLOR);
+  if (img.empty()) {
+    std::cerr << "Failed to open image\n";
+    return 1;
+  }
 
-    int width = img.cols;
-    int height = img.rows;
-
-    // Create a byte matrix [height][width][3] using cv::Vec3b
-    std::vector<std::vector<cv::Vec3b>> matrix(height, std::vector<cv::Vec3b>(width));
-
-    for (int y = 0; y < height; ++y) {
-        const cv::Vec3b* row = img.ptr<cv::Vec3b>(y);
-        for (int x = 0; x < width; ++x) {
-            matrix[y][x] = row[x]; // BGR
-        }
-    }
-
-    // Display the image
-    cv::imshow("Image", img);
-    cv::waitKey(0);
-    return 0;
+  // Display the image
+  cv::imshow("Image", img);
+  cv::waitKey(0);
+  return 0;
 }
