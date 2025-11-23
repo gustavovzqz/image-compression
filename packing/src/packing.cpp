84c76ef29188637d7e9bf64e4fac566a3e1fb955
Gustavo Vazquez <gustavovzqz.dev@gmail.com>
2025-11-23 18:38:31 -0300

    ajuste no rle

--- packing/src/packing.cpp
+++ packing/src/packing.cpp
@@ -1,6 +1,5 @@
 #include <fstream>
 #include <iostream>
-#include <opencv2/core.hpp>
 #include <opencv2/opencv.hpp>
 
 uint16_t packHSV(uchar h, uchar s, uchar v) {
@@ -28,60 +27,72 @@ void writeImageBinaryHSV(cv::Mat &imgBGR) {
   int height = imgHSV.rows;
 
   std::ofstream file("bits.bin", std::ios::binary);
-  if (!file)
+  if (!file) {
+    std::cerr << "Erro ao abrir arquivo para escrita.\n";
     return;
+  }
 
   file.write(reinterpret_cast<char *>(&width), sizeof(int));
   file.write(reinterpret_cast<char *>(&height), sizeof(int));
 
+  uint16_t prevPacked =
+      packHSV(imgHSV.at<cv::Vec3b>(0, 0)[0], imgHSV.at<cv::Vec3b>(0, 0)[1],
+              imgHSV.at<cv::Vec3b>(0, 0)[2]);
+
+  // começar com 0 pq vai repetir 100% na primeira
+  // e precisa ser 16 bits pq quando chegar em 256 não vai zerar
+  uint16_t runLength = 0;
+
   for (int y = 0; y < height; ++y) {
     cv::Vec3b *row = imgHSV.ptr<cv::Vec3b>(y);
-    uint16_t prevPacked = packHSV(row[0][0], row[0][1], row[0][2]);
-    uint16_t runLength = 1;
 
-    for (int x = 1; x <= width; ++x) {
-      uint16_t packed =
-          (x < width) ? packHSV(row[x][0], row[x][1], row[x][2]) : 0xFFFF;
+    for (int x = 0; x < width; ++x) {
+      uint16_t packed = packHSV(row[x][0], row[x][1], row[x][2]);
 
       if (packed == prevPacked && runLength < 255) {
         runLength++;
       } else {
         file.write(reinterpret_cast<char *>(&prevPacked), sizeof(uint16_t));
         file.write(reinterpret_cast<char *>(&runLength), sizeof(uint8_t));
-        runLength = 1;
         prevPacked = packed;
+        runLength = 1;
       }
     }
   }
+
+  file.write(reinterpret_cast<char *>(&prevPacked), sizeof(uint16_t));
+  file.write(reinterpret_cast<char *>(&runLength), sizeof(uint8_t));
 
   file.close();
 }
 
 void readImageBinaryHSV() {
   std::ifstream file("bits.bin", std::ios::binary);
-  if (!file)
+  if (!file) {
+    std::cerr << "Erro ao abrir arquivo para leitura.\n";
     return;
+  }
 
   int width, height;
   file.read(reinterpret_cast<char *>(&width), sizeof(int));
   file.read(reinterpret_cast<char *>(&height), sizeof(int));
 
   cv::Mat imgHSV(height, width, CV_8UC3);
-  int x = 0, y = 0;
 
-  while (file.peek() != EOF && y < height) {
+  int totalPixels = width * height;
+  int idx = 0;
+
+  while (idx < totalPixels && file) {
     uint16_t packed;
     uint8_t runLength;
+
     file.read(reinterpret_cast<char *>(&packed), sizeof(uint16_t));
     file.read(reinterpret_cast<char *>(&runLength), sizeof(uint8_t));
 
-    for (int i = 0; i < runLength; ++i) {
+    for (int i = 0; i < runLength && idx < totalPixels; ++i, ++idx) {
+      int y = idx / width;
+      int x = idx % width;
       imgHSV.at<cv::Vec3b>(y, x) = unpackHSV(packed);
-      x++;
-      if (x >= width) {
-        x = 0;
-        y++;
-      }
     }
   }
 
@@ -90,6 +101,6 @@ void readImageBinaryHSV() {
   cv::Mat imgBGR;
   cv::cvtColor(imgHSV, imgBGR, cv::COLOR_HSV2BGR);
 
-  cv::imshow("Image", imgBGR);
+  cv::imshow("Imagem Recuperada", imgBGR);
   cv::waitKey(0);
 }
