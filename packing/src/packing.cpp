6b6f032b7df8da091e1f3bed9f4166aab8320af5
Gustavo Vazquez <gustavovzqz.dev@gmail.com>
2025-11-23 21:06:47 -0300

    run length com threshold

--- packing/src/packing.cpp
+++ packing/src/packing.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <opencv2/opencv.hpp>
@@ -17,6 +18,25 @@ cv::Vec3b unpackHSV(uint16_t packed) {
   uchar s = (uchar)(S4 << 4);
   uchar v = (uchar)(V6 << 2);
   return cv::Vec3b(h, s, v);
+}
+
+bool closeEnough(uint16_t src, uint16_t dest) {
+
+  cv::Vec3b src_vec = unpackHSV(src);
+  cv::Vec3b dest_vec = unpackHSV(dest);
+
+  uchar h_diff = abs(src_vec[0] - dest_vec[0]);
+  uchar s_diff = abs(src_vec[1] - dest_vec[1]);
+  uchar v_diff = abs(src_vec[2] - dest_vec[2]);
+
+  bool a = (v_diff < 20 && s_diff < 20 && h_diff < 20);
+
+  if (!a) {
+    // std::cout << "Hd:" << (int)h_diff << " ";
+    // std::cout << "Sd:" << (int)s_diff << " ";
+    // std::cout << "Vd:" << (int)v_diff << "\n";
+  }
+  return a;
 }
 
 void writeImageBinaryHSV(cv::Mat &imgBGR) {
@@ -43,20 +63,36 @@ void writeImageBinaryHSV(cv::Mat &imgBGR) {
   // e precisa ser 16 bits pq quando chegar em 256 não vai zerar
   uint16_t runLength = 0;
 
+  std::vector<uint16_t> packedValues;
+
+  uint32_t h_sum = 0;
+  uint32_t s_sum = 0;
+  uint32_t v_sum = 0;
+
   for (int y = 0; y < height; ++y) {
     cv::Vec3b *row = imgHSV.ptr<cv::Vec3b>(y);
 
     for (int x = 0; x < width; ++x) {
-      uint16_t packed = packHSV(row[x][0], row[x][1], row[x][2]);
+      uchar h = row[x][0];
+      uchar s = row[x][1];
+      uchar v = row[x][2];
 
-      if (packed == prevPacked && runLength < 255) {
+      uint16_t packed = packHSV(h, s, v);
+
+      if (closeEnough(packed, prevPacked) && runLength < 255) {
         runLength++;
       } else {
-        file.write(reinterpret_cast<char *>(&prevPacked), sizeof(uint16_t));
+        uint16_t packedMean =
+            packHSV(h_sum / runLength, s_sum / runLength, v_sum / runLength);
+        file.write(reinterpret_cast<char *>(&packedMean), sizeof(uint16_t));
         file.write(reinterpret_cast<char *>(&runLength), sizeof(uint8_t));
         prevPacked = packed;
+        h_sum = s_sum = v_sum = 0;
         runLength = 1;
       }
+      h_sum += h;
+      s_sum += s;
+      v_sum += v;
     }
   }
 
