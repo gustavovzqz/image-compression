2ed200ec17e02e0d57bfa4c30ef21f91a623c54b
Gustavo Vazquez <gustavovzqz.dev@gmail.com>
2025-11-24 21:36:22 -0300

    salvando como bmp

--- decode/decode.cpp
+++ decode/decode.cpp
@@ -2,28 +2,32 @@
 #include "packing/include/packing.hpp"
 #include <filesystem>
 #include <iostream>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgcodecs.hpp>
 #include <opencv2/opencv.hpp>
+
 namespace fs = std::filesystem;
 
 fs::path TEMP_FILE = "temp.bin";
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "Uso: " << argv[0] << " <imagem_comprimida> \n";
+  if (argc != 3) {
+    std::cerr << "Uso: " << argv[0] << " <imagem_comprimida> <saida.bmp>\n";
     return 1;
   }
 
-  std::string inputImage = argv[1];
+  std::string inputCompressed = argv[1];
+  std::string outputBmp = argv[2];
 
-  reconstructOriginal(inputImage, TEMP_FILE);
+  reconstructOriginal(inputCompressed, TEMP_FILE);
 
   cv::Mat output_img = uncompressBinary(TEMP_FILE);
   fs::remove(TEMP_FILE);
-  cv::imshow("Imagem: ", output_img);
-  cv::waitKey(0);
+
+  if (output_img.empty()) {
+    std::cerr << "Erro: imagem reconstruída vazia\n";
+    return 1;
+  }
+
+  cv::imwrite(outputBmp, output_img);
 
   return 0;
 }
